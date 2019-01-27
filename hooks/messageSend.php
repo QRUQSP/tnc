80f386a0a29d158be6a5759c1883cb333fdd1bfa
Andrew Rivett <veggiefrog@gmail.com>
2019-01-27 16:04:14 +0000

    Working on symlink issue

--- hooks/messageSend.php
+++ hooks/messageSend.php
@@ -54,7 +54,6 @@ function qruqsp_tnc_hooks_messageSend(&$ciniki, $tnid, $args) {
     // Parse the callsign of the next callsign to send to
     //
     list($callsign, $ssid) = explode('-', $args['next_callsign'] . '-');
-    error_log('to: ' . $callsign . strlen($callsign));
     for($i = 0; $i < 6; $i++) {
         if( $i < strlen($callsign) ) {
             $str .= pack('C', ord($callsign[$i])<<1);
@@ -72,7 +71,6 @@ function qruqsp_tnc_hooks_messageSend(&$ciniki, $tnid, $args) {
     // Parse the from callsign
     //
     list($callsign, $ssid) = explode('-', $args['from_callsign'] . '-');
-    error_log('from: ' . $callsign . strlen($callsign));
     for($i = 0; $i < 6; $i++) {
         if( $i < strlen($callsign) ) {
             $str .= pack('C', ord($callsign[$i])<<1);
@@ -85,7 +83,6 @@ function qruqsp_tnc_hooks_messageSend(&$ciniki, $tnid, $args) {
         // Add 0x01 to signal the end of the callsigns
         // FIXME: Remove 0x01 when digipeater addresses are included
         $str .= pack('C', ((($ssid&0x0f)<<1)|0x01) );
-//        $str .= pack('C', 0x01);
     } else {
         $str .= pack('C', 0x01);
     }
@@ -98,9 +95,7 @@ function qruqsp_tnc_hooks_messageSend(&$ciniki, $tnid, $args) {
     //
     // Setup message in APRS format
     //
-    error_log($args['content']);
     $message = sprintf(":%-9s:%s", $args['to_callsign'], $args['content']);
-    error_log($message);
     $str .= pack('a*', $message);
 
     //
@@ -108,24 +103,23 @@ function qruqsp_tnc_hooks_messageSend(&$ciniki, $tnid, $args) {
     //
     $str .= pack('C', 0xC0);
     
-    error_log(file_put_contents('/tmp/str', $str));
-    error_log(file_put_contents('/ciniki/sites/qruqsp.local/logs/str', $str));
+    $pts_filename = $ciniki['config']['qruqsp.tnc']['pts'];
 
-    if( is_link($ciniki['config']['qruqsp.tnc']['pts']) ) {
-        $ciniki['config']['qruqsp.tnc']['pts'] = readlink($ciniki['config']['qruqsp.tnc']['pts']);
+    if( is_link($pts_filename) ) {
+        error_log('symlink');
+        $pts_filename = readlink($pts_filename);
+    } else {
+        error_log('no link');
     }
 
-    if( !file_exists($ciniki['config']['qruqsp.tnc']['pts']) ) {
+    if( !file_exists($pts_filename) ) {
         return array('stat'=>'fail', 'err'=>array('code'=>'qruqsp.sams.15', 'msg'=>'Unable to open tnc'));
     }
 
     //
     // Write to the tnc
     //
-    error_log('sending packet: ' . $ciniki['config']['qruqsp.tnc']['pts']);
-
-    //$pts_handle = fopen($ciniki['config']['qruqsp.tnc']['pts'], 'wb');
-    $pts_handle = fopen('/dev/pts/4', 'wb');
+    $pts_handle = fopen($pts_filename, 'wb');
     if( $pts_handle === false ) {
         return array('stat'=>'fail', 'err'=>array('code'=>'qruqsp.sams.14', 'msg'=>'Unable to open tnc'));
     }
