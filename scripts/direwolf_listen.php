79dde458e127e99d27651cc6e8803d9bad6fe19d
Andrew Rivett <veggiefrog@gmail.com>
2020-06-06 13:34:41 +0000

    Updated direwolf listener to properly set mic volume

--- scripts/direwolf_listen.php
+++ scripts/direwolf_listen.php
@@ -136,7 +136,7 @@ $ciniki['tenant']['modules'] = $rc['modules'];
 // Parent process
 //
 if( $pid > 0 ) {
-    exec('amixer -c 1 set Mic 0db');
+    exec('amixer -c 1 set Mic 100%');
     exec('amixer -c 1 set Headphone 50%');
 
     //
